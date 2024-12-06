666d39cac3a1e1c874fe39c1ed1d6c31e43c61e3
axel <ssaxel03@gmail.com>
2024-12-06 11:39:04 +0000

    feat: initial project skeleton

--- portfolio-website/src/app/layout.tsx
+++ portfolio-website/src/app/layout.tsx
@@ -2,6 +2,27 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7xgPNMHyCNdaZQB0XiVn6tYIVXrzXlBw",
+  authDomain: "portfolio-website-6c1e9.firebaseapp.com",
+  projectId: "portfolio-website-6c1e9",
+  storageBucket: "portfolio-website-6c1e9.firebasestorage.app",
+  messagingSenderId: "188793124382",
+  appId: "1:188793124382:web:ab21001b502aaa688b600f",
+  measurementId: "G-RPGLQZRHP3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
